e8b993162559b68f32543d0bf5a9f6cb0f0bb313
rajgupta04 <engineerrajgupta@gmail.com>
2026-03-25 14:07:16 +0530

    Initial commit

---
+++ first/resources/views/student3.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student 3</title>
+</head>
+<body>
+    <h1>Student 3 View</h1>
+    <p>Welcome! You passed the age check.</p>
+    <ul>
+        <li><strong>Name:</strong> {{ $name }}</li>
+        <li><strong>Roll No:</strong> {{ $roll }}</li>
+        <li><strong>Section:</strong> {{ $section }}</li>
+    </ul>
+</body>
+</html>
---
+++ first/routes/web.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use function PHPUnit\Framework\isNumeric;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/json', function () {
+    return response()->JSON(["Message"=>"How Are You?"]);
+});
+// Route::get('/{name}', function ($name) {
+//     return response()->JSON(["Message"=>"Hello $name"]);
+// });
+Route::get('user/name/{name}', function ($name) {
+    return view('user',['n'=>$name]);
+})->where('name', '[A-Z][a-z]+'); // '[]' -> single '[]+' -> mutiple any no of time '[]*' -> multiple but unique digits  where('name', '[A-Za-z\s\-]+');
+// Route::get('/{id}', function ($id) {
+//     return view('user',['n'=>$id]);
+// })->whereNumber('id');
+Route::get('user/idname/{id}/{name}', function ($id,$name) {
+    return view('user',['n'=>$id,'id'=>$id]);
+})->where('name', '[A-Z][a-z]+')->where('id', '[0-9]+');
+Route::get('user/id/{id}', function ($id) {
+    if(is_numeric($id))
+    return view('user',['n'=>$id]);
+else return "Only Integer Are Allowed!!";
+});
+
+Route::get('student',function(){
+    [$name='Parveen',$roll=12,$section="323Ft"];
+    return view('student1',['name' =>$name,'roll'=>$roll,'section' => $section]);
+});
+
+Route::middleware([\App\Http\Middleware\CheckAge::class])->group(function () {
+    Route::get('/student1', function () {
+        return view('student1', [
+            'name' => fake()->name(),
+            'roll' => fake()->numberBetween(1, 100),
+            'section' => fake()->randomElement(['A', 'B', 'C', 'D'])
+        ]);
+    });
+    
+    Route::get('/student2', function () {
+        return view('student2', [
+            'name' => fake()->name(),
+            'roll' => fake()->numberBetween(1, 100),
+            'section' => fake()->randomElement(['A', 'B', 'C', 'D'])
+        ]);
+    });
+    
+    Route::get('/student3', function () {
+        return view('student3', [
+            'name' => fake()->name(),
+            'roll' => fake()->numberBetween(1, 100),
+            'section' => fake()->randomElement(['A', 'B', 'C', 'D'])
+        ]);
+    });
+});
